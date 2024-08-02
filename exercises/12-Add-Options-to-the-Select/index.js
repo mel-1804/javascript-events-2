2cfd4c535c190484cf6019f9bca169215572430a
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-02 19:02:36 -0400

    terminado

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,5 +1,18 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
 // Your code here
-let input = "option"
-input.appendChild(countries)
+let input = document.getElementById("mySelect");
+
+for (let i = 0; i < countries.length; i++){
+    let option = document.createElement("option");
+    option.value = countries[i];
+    option.innerHTML = countries[i];
+    input.appendChild(option);
+    
+}
+
+input.addEventListener("change", function () {
+    let pais = input.value;
+    alert(pais);
+});
+
--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,35 @@
 // Your code here
+const deleteLi = document.querySelectorAll("li");
+
+for (let i = 0; i < deleteLi.length; i++) {
+    deleteLi[i].firstElementChild.firstElementChild.addEventListener("click", (e) => {
+        // console.log(e.target.parentElement)
+        // console.log(deleteLi[i].firstElementChild.firstElementChild)
+        e.target.parentElement.parentElement.remove()
+    })
+}
+
+
+
+
+
+const inputTarea = document.getElementById("addToDo");
+let myList = document.querySelector("ul");
+const newItem = document.createElement("li");
+
+inputTarea.addEventListener("change", (e) => {
+    newItem.innerHTML = `<span><i class="fa fa-trash"></i></span> ${inputTarea.value}`;
+    myList.appendChild(newItem);
+    // console.log(inputTarea)
+})
+
+inputTarea.addEventListener("keypress", (e) => {
+    if(e.key = "Enter") {
+    newItem.innerHTML = `<span><i class="fa fa-trash"></i></span> ${inputTarea.value}`;
+    myList.appendChild(newItem);
+    }
+// console.log(e.key)
+      
+    
+    
+})
